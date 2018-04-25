68e74116feae0e88517dc4d878f703c909af7ec8
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-04-25 21:05:46 +0100

    Create Empty values.py

---
+++ Python Scripts/Empty values.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import numpy as np 
+import seaborn as sns
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('Iris head.csv')
+
+print(df.isnull().any())
