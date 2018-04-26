3f7816cb57f429a75d6e950e060a2a745118703d
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-04-26 21:04:50 +0100

    New script

---
+++ Python Scripts/QuickAnalysis.py
@@ -0,0 +1,10 @@
+import numpy as np
+import scipy as sp
+import pandas as pdIn [4]: import matplotlib as mpl
+import seaborn as sns
+
+df = pd.read_csv('Iris head.csv')In [7]: 
+
+df.mean()
+df.median()
+df.std()
