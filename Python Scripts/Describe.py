b974e7505b0763092589b7a1999ed30113a6843c
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-04-28 17:17:37 +0100

    Added comments

--- Python Scripts/Describe.py
+++ Python Scripts/Describe.py
@@ -1,8 +1,11 @@
-import pandas as pd 
-import numpy as np 
-import matplotlib as pl 
-import matplotlib.pyplot as mpl 
+# Ger Hanlon, 13.04.2018
+# The describe function for the 2018 Iris Data-Set project
 
-df = pd.read_csv('Iris head.csv')
+import pandas as pd # Import the panda library and reference it is pd
+import numpy as np # Import the numpy library and reference it is np
+import matplotlib as pl #Import the matplot library and reference it is pl
+import matplotlib.pyplot as mpl #Import the plotting framework from matplot and reference it as mpl 
 
-print(df.describe())
+df = pd.read_csv('Iris head.csv') #read the csv file Iris Head into the dataframe(df)
+
+print(df.describe()) #Print the describe function from the dataframe
