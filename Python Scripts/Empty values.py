928b9854beab293bb785cc9845040a24ff43e4e2
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-04-28 17:10:10 +0100

    Delete Empty values.py

--- Python Scripts/Empty values.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import numpy as np 
-import seaborn as sns
-import matplotlib.pyplot as plt 
-from sklearn.model_selection import train_test_split
-
-df = pd.read_csv('Iris head.csv')
-
-print(df.isnull().any())
