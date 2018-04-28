f245b2a91d2274ee3e7c57afd6614d2066dd3fe4
Gerrydh <35727450+Gerrydh@users.noreply.github.com>
2018-04-28 17:25:35 +0100

    Added comments

--- Python Scripts/Max and Min by species.py
+++ Python Scripts/Max and Min by species.py
@@ -2,14 +2,15 @@
 # This code returns the smallest and the largest sepal and petal lenghts and widths by species
 # https://stackoverflow.com/questions/49689028/python-iris-data-set-include-the-species/49689170#49689170
 
-import pandas as pd  #Import Pandas
-import numpy as np   # Import Numpy
+import pandas as pd  # Import the panda library and reference it is pd- This library is used for data manipulation and analysis
+import numpy as np   # Import the numpy library and reference it is np- This library is useful for adding support for large, multi-dimensional 
+                     # arrays and matrices, along with a large collection of high-level mathematical functions to operate on these arrays.
 
 
 
-df = pd.read_csv("iris head.csv")
+df = pd.read_csv("iris head.csv") #read the csv file Iris Head into the dataframe(df)
 
-print('The biggest and smallest Sepal Lenght is:', df.groupby(['Species'])['Sepal Length'].agg(['max','min']))
-print('The biggest and smallest Sepal Width is:',df.groupby(['Species'])['Sepal Width'].agg(['max','min']))
-print('The biggest and smallest Petal Lenght is:',df.groupby(['Species'])['Petal length'].agg(['max','min']))
-print('The biggest and smallest Petal Width is:',df.groupby(['Species'])['Petal Width'].agg(['max','min']))
+print('The biggest and smallest Sepal Lenght is:', df.groupby(['Species'])['Sepal Length'].agg(['max','min'])) #Prints the biggest and smallest sepal lenghts including the species
+print('The biggest and smallest Sepal Width is:',df.groupby(['Species'])['Sepal Width'].agg(['max','min'])) #Prints the biggest and smallest sepal widths including the species
+print('The biggest and smallest Petal Lenght is:',df.groupby(['Species'])['Petal length'].agg(['max','min'])) #Prints the biggest and smallest petal lenghts including the species
+print('The biggest and smallest Petal Width is:',df.groupby(['Species'])['Petal Width'].agg(['max','min'])) #Prints the biggest and smallest petal widths including the species
